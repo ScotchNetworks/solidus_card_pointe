dd91f636d2d52f82ddfe083105da25b6c75eded6
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2024-11-12 15:52:51 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+See https://github.com/solidusio-contrib/solidus_card_pointe/releases or OLD_CHANGELOG.md for older versions.
---
+++ lib/solidus_card_pointe.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'solidus_card_pointe/configuration'
+require 'solidus_card_pointe/version'
+require 'solidus_card_pointe/engine'
