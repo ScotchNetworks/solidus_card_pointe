1b6f2c2bb5c2da7da0dcd5133ec06399d143db54
safa <aballaghsafa@gmail.com>
2024-11-25 21:03:12 +0100

    Implement authorize method

---
+++ app/services/card_pointe/transactions/authorize_service.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module CardPointe
+  module Transactions
+    class AuthorizeService < CardPointe::Transactions::BaseService
+      def initialize(order, payment_source, payment_method)
+        @order = order
+        @payment_source = payment_source
+        super(payment_method)
+      end
+
+      def call
+        authorize
+      end
+
+      private
+
+      def authorize
+        url = "#{card_pointe_url}/auth"
+
+        body = {
+          merchid: card_pointe_merchant_id,
+          account: @payment_source.card_token,
+          amount: @order.amount.to_f,
+          currency: @order.currency,
+          capture: 'N',
+        }
+
+        headers = {
+          'Authorization' => card_pointe_authorization,
+          'Content-Type' => 'application/json',
+        }
+
+        response = HTTParty.post(url, body: body.to_json, headers: headers)
+        handle_response(response)
+      end
+    end
+  end
+end
