5fb2911539c8dc879d178713f6e3dc6a45e4c568
safa <aballaghsafa@gmail.com>
2024-12-03 16:56:42 +0100

    Save and show card information for card pointe payment sources

---
+++ db/migrate/20241203145821_add_card_information_to_payment_source.rb
@@ -0,0 +1,6 @@
+class AddCardInformationToPaymentSource < ActiveRecord::Migration[7.2]
+  def change
+    add_column :solidus_card_pointe_payment_sources, :card_last4, :string
+    add_column :solidus_card_pointe_payment_sources, :card_type, :string
+  end
+end
